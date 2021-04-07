d7a3415116ad52ba5aaecbba2bcfdd5fb4423a4a
F4lanker <nikberezovskiy@gmail.com>
2021-04-07 11:35:51 +0300

    add main cycle and eyes frames

--- Pygame/pyg_smile.py
+++ Pygame/pyg_smile.py
@@ -5,11 +5,16 @@ pygame.init()
 
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
-
+white = (255, 255, 255)
 yellow = (255, 255, 0)
+red = (255, 0, 0)
+
+screen.fill(white)
 
 # здесь будут рисоваться фигуры
 circle(screen, yellow, (200, 200), 150)
+circle(screen, red, (140, 160), 27)
+circle(screen, red, (250, 160), 27)
 
 pygame.display.update()
 clock = pygame.time.Clock()
