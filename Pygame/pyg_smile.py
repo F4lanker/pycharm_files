8b32b5682bfc98e39be8d9f4b6db9d5cacb3ecd3
F4lanker <nikberezovskiy@gmail.com>
2021-04-07 11:59:13 +0300

    refactoring with function drawing black frames

--- Pygame/pyg_smile.py
+++ Pygame/pyg_smile.py
@@ -6,15 +6,23 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 white = (255, 255, 255)
+black = (0, 0, 0)
 yellow = (255, 255, 0)
 red = (255, 0, 0)
+
+def circ(color, coordinat, radius):
+    circle(screen, color, coordinat, radius)  # main circle
+    circle(screen, black, coordinat, radius + 1, 1)
 
 screen.fill(white)
 
 # здесь будут рисоваться фигуры
-circle(screen, yellow, (200, 200), 150)
-circle(screen, red, (140, 160), 27)
-circle(screen, red, (250, 160), 27)
+circ(yellow, (200, 200), 150)  # Face frame
+circ(red, (140, 160), 30)  # Left eye frame
+circ(red, (250, 160), 26)  # Right eye frame
+circ(black, (140, 160), 14)
+circ(black, (250, 160), 12)
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
