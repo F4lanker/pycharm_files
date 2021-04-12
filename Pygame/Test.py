c1f94389c21d02775485c740d6a163062b325389
F4lanker <nikberezovskiy@gmail.com>
2021-04-12 13:19:27 +0300

    add corrections

--- Pygame/Test.py
+++ Pygame/Test.py
@@ -8,24 +8,28 @@ win = pygame.display.set_mode((500, 500))
 x = 50
 y = 50
 width = 40
-height =60
-speed =5
+height = 60
+speed = 5
+isJump = False
+Jumpcount = 10
 
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x >= 5:
         x -= speed
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 500 - width - 5:
         x += speed
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and y >= 5:
         y -= speed
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and y < 500 - height - 5:
         y += speed
+    if keys[pygame.K_SPACE]:
+        isJump is True
 
     win.fill(cl.black)
     rect(win, cl.blue, (x, y, width, height))
--- Pygame/pyg_smile.py
+++ Pygame/pyg_smile.py
@@ -2,17 +2,25 @@ import pygame
 from pygame.draw import *
 
 pygame.init()
-
+pygame.display.set_caption('Angry smile')
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 white = (255, 255, 255)
 black = (0, 0, 0)
 yellow = (255, 255, 0)
 red = (255, 0, 0)
+x = 200
+y = 200
+speed = 5
 
 def circ(color, coordinat, radius):
     circle(screen, color, coordinat, radius)  # main circle
     circle(screen, black, coordinat, radius, 1)
+
+
+#def brow(start_pos, end_pos):
+    #line(screen, black, start_pos, end_pos)
+
 
 screen.fill(white)
 
@@ -23,17 +31,37 @@ circ(red, (250, 160), 26)  # Right eye frame
 circ(black, (140, 160), 14)  # Eye center left
 circ(black, (250, 160), 12)  # Eye center right
 rect(screen, black, (150, 250, 100, 36))  # Mouth
+#brow(120, 250)
+
 rect(screen, red, (100, 220, 150, 25), 0, -28, -30)
 
 
 
-pygame.display.update()
-clock = pygame.time.Clock()
 finished = False
 
 while not finished:
-    clock.tick(FPS)
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x -= speed
+    if keys[pygame.K_RIGHT]:
+        x += speed
+    if keys[pygame.K_UP]:
+        y -= speed
+    if keys[pygame.K_DOWN]:
+        y += speed
+    if keys[pygame.K_SPACE]:
+        print('Cuurent X:', x, 'Current Y:', y)
+
+    circle(screen, black, (x, y), 2)
+    pygame.display.update()
+
+
+pygame.quit()
